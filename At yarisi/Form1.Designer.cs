d8632ef0fca3fa25945124b0ff301dd5e0b14f2f
Cengizhan KOÇAL <cngzkcl53@gmail.com>
2020-12-22 14:19:26 +0300

    Initial commit

---
+++ At yarisi/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace At_yarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblbitis = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.at1 = new System.Windows.Forms.PictureBox();
+            this.at2 = new System.Windows.Forms.PictureBox();
+            this.at3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblbaslangic = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.at4 = new System.Windows.Forms.PictureBox();
+            this.at5 = new System.Windows.Forms.PictureBox();
+            this.at6 = new System.Windows.Forms.PictureBox();
+            this.lblekran = new System.Windows.Forms.Label();
+            this.zaman = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.at1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1169, 20);
+            this.label1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1169, 20);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(-1, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1169, 20);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(-1, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1169, 20);
+            this.label4.TabIndex = 3;
+            // 
+            // lblbitis
+            // 
+            this.lblbitis.BackColor = System.Drawing.Color.White;
+            this.lblbitis.Location = new System.Drawing.Point(1164, -2);
+            this.lblbitis.Name = "lblbitis";
+            this.lblbitis.Size = new System.Drawing.Size(29, 539);
+            this.lblbitis.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Blue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(2, 540);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "BASLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(-1, 517);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(1169, 20);
+            this.label6.TabIndex = 6;
+            // 
+            // at1
+            // 
+            this.at1.Image = ((System.Drawing.Image)(resources.GetObject("at1.Image")));
+            this.at1.Location = new System.Drawing.Point(2, 21);
+            this.at1.Name = "at1";
+            this.at1.Size = new System.Drawing.Size(94, 60);
+            this.at1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.at1.TabIndex = 7;
+            this.at1.TabStop = false;
+            // 
+            // at2
+            // 
+            this.at2.Image = ((System.Drawing.Image)(resources.GetObject("at2.Image")));
+            this.at2.Location = new System.Drawing.Point(2, 107);
+            this.at2.Name = "at2";
+            this.at2.Size = new System.Drawing.Size(94, 60);
+            this.at2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.at2.TabIndex = 8;
+            this.at2.TabStop = false;
+            // 
+            // at3
+            // 
+            this.at3.Image = ((System.Drawing.Image)(resources.GetObject("at3.Image")));
+            this.at3.Location = new System.Drawing.Point(2, 192);
+            this.at3.Name = "at3";
+            this.at3.Size = new System.Drawing.Size(94, 60);
+            this.at3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.at3.TabIndex = 9;
+            this.at3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblbaslangic
+            // 
+            this.lblbaslangic.BackColor = System.Drawing.Color.White;
+            this.lblbaslangic.Location = new System.Drawing.Point(102, 18);
+            this.lblbaslangic.Name = "lblbaslangic";
+            this.lblbaslangic.Size = new System.Drawing.Size(11, 519);
+            this.lblbaslangic.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(-1, 345);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(1169, 20);
+            this.label10.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(-1, 431);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(1169, 20);
+            this.label9.TabIndex = 12;
+            // 
+            // at4
+            // 
+            this.at4.Image = ((System.Drawing.Image)(resources.GetObject("at4.Image")));
+            this.at4.Location = new System.Drawing.Point(2, 282);
+            this.at4.Name = "at4";
+            this.at4.Size = new System.Drawing.Size(94, 60);
+            this.at4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.at4.TabIndex = 15;
+            this.at4.TabStop = false;
+            // 
+            // at5
+            // 
+            this.at5.Image = ((System.Drawing.Image)(resources.GetObject("at5.Image")));
+            this.at5.Location = new System.Drawing.Point(2, 368);
+            this.at5.Name = "at5";
+            this.at5.Size = new System.Drawing.Size(94, 60);
+            this.at5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.at5.TabIndex = 16;
+            this.at5.TabStop = false;
+            // 
+            // at6
+            // 
+            this.at6.Image = ((System.Drawing.Image)(resources.GetObject("at6.Image")));
+            this.at6.Location = new System.Drawing.Point(2, 454);
+            this.at6.Name = "at6";
+            this.at6.Size = new System.Drawing.Size(94, 60);
+            this.at6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.at6.TabIndex = 17;
+            this.at6.TabStop = false;
+            // 
+            // lblekran
+            // 
+            this.lblekran.BackColor = System.Drawing.Color.Black;
+            this.lblekran.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblekran.ForeColor = System.Drawing.Color.White;
+            this.lblekran.Location = new System.Drawing.Point(289, 540);
+            this.lblekran.Name = "lblekran";
+            this.lblekran.Size = new System.Drawing.Size(777, 47);
+            this.lblekran.TabIndex = 18;
+            this.lblekran.Text = "Hazir";
+            this.lblekran.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // zaman
+            // 
+            this.zaman.BackColor = System.Drawing.Color.Black;
+            this.zaman.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.zaman.ForeColor = System.Drawing.Color.White;
+            this.zaman.Location = new System.Drawing.Point(1072, 537);
+            this.zaman.Name = "zaman";
+            this.zaman.Size = new System.Drawing.Size(143, 53);
+            this.zaman.TabIndex = 19;
+            this.zaman.Text = "0";
+            this.zaman.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1366, 596);
+            this.Controls.Add(this.zaman);
+            this.Controls.Add(this.lblekran);
+            this.Controls.Add(this.at6);
+            this.Controls.Add(this.at5);
+            this.Controls.Add(this.at4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblbaslangic);
+            this.Controls.Add(this.at3);
+            this.Controls.Add(this.at2);
+            this.Controls.Add(this.at1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblbitis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.at1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.at6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblbitis;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox at1;
+        private System.Windows.Forms.PictureBox at2;
+        private System.Windows.Forms.PictureBox at3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblbaslangic;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox at4;
+        private System.Windows.Forms.PictureBox at5;
+        private System.Windows.Forms.PictureBox at6;
+        private System.Windows.Forms.Label lblekran;
+        private System.Windows.Forms.Label zaman;
+    }
+}
+
---
+++ At yarisi/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace At_yarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int birinciatinsoluzaklik, ikinciatinsoluzaklik, ucuncuatinsoluzaklik, dorduncuatinsoluzaklik, besinciatincoluzaklik, altinciatinsoluzaklik;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            lblbaslangic.Visible = false;
+            lblekran.Text = "Yaris basladi!!!";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int derece = Convert.ToInt32(zaman.Text);
+            derece++;          
+            zaman.Text = derece.ToString();
+            int birinciatingenisligi = at1.Width;
+            int ikinciatingenisligi = at2.Width;
+            int ucuncuatingenisligi = at3.Width;
+            int dorduncuatingenisligi = at4.Width;
+            int besinciatingenisligi = at5.Width;
+            int altinciatingenisligi = at6.Width;
+
+            int bitisuzakligi = lblbitis.Left;
+
+            at1.Left += rastgele.Next(5, 15);
+            at2.Left += rastgele.Next(5, 15);
+            at3.Left += rastgele.Next(5, 15);
+            at4.Left += rastgele.Next(5, 15);
+            at5.Left += rastgele.Next(5, 15);
+            at6.Left += rastgele.Next(5, 15);
+
+            if(at1.Left>at2.Left+5&& at1.Left > at3.Left + 5 && at1.Left > at4.Left + 5 && at1.Left > at5.Left + 5 && at1.Left > at6.Left + 5 )
+            {
+                lblekran.Text = "1. at liderligi ele gecirdi";
+            }
+            if (at2.Left > at1.Left + 5 && at2.Left > at3.Left + 5 && at2.Left > at4.Left + 5 && at2.Left > at5.Left + 5 && at2.Left > at6.Left + 5)
+            {
+                lblekran.Text = "2. at liderligi ele gecirdi";
+            }
+            if (at3.Left > at1.Left + 5 && at3.Left > at2.Left + 5 && at3.Left > at4.Left + 5 && at3.Left > at5.Left + 5 && at3.Left > at6.Left + 5)
+            {
+                lblekran.Text = "3. at liderligi ele gecirdi";
+            }
+            if (at4.Left > at1.Left + 5 && at4.Left > at2.Left + 5 && at4.Left > at3.Left + 5 && at4.Left > at5.Left + 5 && at4.Left > at6.Left + 5)
+            {
+                lblekran.Text = "4. at liderligi ele gecirdi";
+            }
+            if (at5.Left > at1.Left + 5 && at5.Left > at2.Left + 5 && at5.Left > at3.Left + 5 && at5.Left > at4.Left + 5 && at5.Left > at6.Left + 5)
+            {
+                lblekran.Text = "5. at liderligi ele gecirdi";
+            }
+            if (at6.Left > at1.Left + 5 && at6.Left > at2.Left + 5 && at6.Left > at3.Left + 5 && at6.Left > at4.Left + 5 && at6.Left > at5.Left + 5)
+            {
+                lblekran.Text = "6. at liderligi ele gecirdi";
+            }
+
+
+            if (birinciatingenisligi + at1.Left >= bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                lblekran.Text = ("1 Numaralari at yarisi kazandi!!!");                
+            }
+            if (ikinciatingenisligi + at2.Left >= bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                lblekran.Text = ("2 Numaralari at yarisi kazandi!!!");
+            }
+            if (ucuncuatingenisligi + at3.Left >= bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                lblekran.Text = ("3 Numaralari at yarisi kazandi!!!");
+            }
+            if (dorduncuatingenisligi + at4.Left >= bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                lblekran.Text = ("4 Numaralari at yarisi kazandi!!!");
+            }
+            if (besinciatingenisligi + at5.Left >= bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                lblekran.Text = ("5 Numaralari at yarisi kazandi!!!");
+            }
+            if (altinciatingenisligi + at6.Left >= bitisuzakligi)
+            {
+                timer1.Enabled = false;
+                lblekran.Text = ("6 Numaralari at yarisi kazandi!!!");
+            }
+
+        }
+
+        Random rastgele = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            birinciatinsoluzaklik = at1.Left;
+            ikinciatinsoluzaklik = at2.Left;
+            ucuncuatinsoluzaklik = at3.Left;
+            dorduncuatinsoluzaklik = at4.Left;
+            besinciatincoluzaklik = at5.Left;
+            altinciatinsoluzaklik = at6.Left;
+
+        }
+    }
+}
